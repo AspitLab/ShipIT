ae914d8f7f9787d9738979dd9aada58a6df2c7c5
Jakob Moeller <jlm1999@live.dk>
2017-11-22 09:50:36 +0100

    Removed a DB test

--- ShipIt/ShipIt/MainWindow.xaml.cs
+++ ShipIt/ShipIt/MainWindow.xaml.cs
@@ -23,11 +23,6 @@ namespace ShipIt
 		public MainWindow()
 		{
 			InitializeComponent();
-
-			Model m = new Model("XM6", 30, 1000, 200);
-			MessageBox.Show(m.HasId.ToString());
-			m.InsertIntoDB();
-			MessageBox.Show(m.GetId().ToString());
 		}
 
 		private void ButtonCreateModel_Click(object sender, RoutedEventArgs e)
